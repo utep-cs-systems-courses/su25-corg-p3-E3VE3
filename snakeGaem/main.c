3f7890f2ecd319ec2de588c4f20b48076583b546
E3VE3 <141441368+E3VE3@users.noreply.github.com>
2025-07-29 16:45:42 -0600

    Update main.c

--- snakeGaem/main.c
+++ snakeGaem/main.c
@@ -4,33 +4,38 @@
 #include "display.h"
 #include "input.h"
 #include "sound.h"
+#include "utils.h"        // <-- for createNewApple()
 
-void setup() {
-    WDTCTL = WDTPW | WDTTMSEL | WDTCNTCL | WDTSSEL_0 | WDTIS0;
-    IE1 |= WDTIE;
+void setup(void) {
+    // Configure WDT as interval timer: SMCLK/8K, interval ~7.4ms
+    WDTCTL = WDTPW    |  // (bits 15-8) password
+             WDTTMSEL |  // (bit 4) select timer mode
+             WDTCNTCL |  // (bit 3) clear counter
+             WDTIS0;     // (bits 1–0 = 01) divide by 8K
 
-    CCTL0 = CCIE;
-    TACTL = TASSEL_2 + MC_2;
+    IE1 |= WDTIE;      // enable WDT interrupt
+
+    CCTL0 = CCIE;      
+    TACTL = TASSEL_2 | MC_2;  // Timer_A: SMCLK, continuous mode
 
     setupDisplayPins();
     setupInputPins();
     enableDisplay();
     sound_init();
 
-    gameState = STATE_PAUSED;
-    snakeLength = 1;
+    gameState       = STATE_PAUSED;
+    snakeLength     = 1;
     generateInitial();
-    createNewApple();
-    direction = DIR_RIGHT;
+    createNewApple();       // now properly declared
+    direction       = DIR_RIGHT;
     display_counter = 10;
 }
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;
+    WDTCTL = WDTPW | WDTHOLD; // stop WDT for setup
     setup();
     __enable_interrupt();
-
-    while (1) {
-        __bis_SR_register(LPM0_bits + GIE);
+    for (;;) {
+        __bis_SR_register(LPM0_bits | GIE);
     }
 }
